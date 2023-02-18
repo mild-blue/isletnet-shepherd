432481a87164f971fe084384985e85654befd0d4
abragtim <timurkatime@gmail.com>
2023-02-18 10:03:26 +0100

    Add file handlers.

--- shepherd/utils/logging_config.py
+++ shepherd/utils/logging_config.py
@@ -2,12 +2,18 @@ import logging
 
 from logging.config import dictConfig
 
+LOGGING_DIRECTORY = '../logs'
+
 
 LOGGING_CONFIG = {
     "version": 1,
     "disable_existing_loggers": True,
     "formatters": {
         "terminal": {
+            "level": "NOTSET",
+            "format": "%(asctime)s-%(levelname)s-%(name)s::%(module)s|%(lineno)s:: %(message)s"
+        },
+        "file": {
             "level": "NOTSET",
             "format": "%(asctime)s-%(levelname)s-%(name)s::%(module)s|%(lineno)s:: %(message)s"
         }
@@ -18,6 +24,24 @@ LOGGING_CONFIG = {
             "level": "NOTSET",
             "formatter": 'terminal',
             "stream": "ext://sys.stdout"
+        },
+        "info_file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "INFO",
+            "formatter": "file",
+            "filename": f"{LOGGING_DIRECTORY}/info_shepherd.log",
+            "maxBytes": 5000000,
+            "backupCount": 5,
+            "encoding": "utf-8"
+        },
+        "error_file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "ERROR",
+            "formatter": "file",
+            "filename": f"{LOGGING_DIRECTORY}/error_shepherd.log",
+            "maxBytes": 5000000,
+            "backupCount": 5,
+            "encoding": "utf-8"
         }
     },
     "loggers": {
@@ -25,7 +49,7 @@ LOGGING_CONFIG = {
             # root logger
             "level": "NOTSET",
             "handlers": [
-                "console"
+                "console", "info_file", "error_file"
             ],
         },
         "aiohttp.server": {}
