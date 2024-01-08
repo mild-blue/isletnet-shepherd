4877acf037adb1a9a6fd813700b3aa1c8a69b26e
jakubmonhart <monhart.jakub@gmail.com>
2024-01-08 11:20:30 +0100

    Notify about the finished job only once.

--- shepherd/shepherd/shepherd.py
+++ shepherd/shepherd/shepherd.py
@@ -293,6 +293,11 @@ class Shepherd:
                     status.finished_at = datetime.utcnow()
                     await self._job_status_update_queue.enqueue_task(self._storage.set_job_status(job_id, status.copy()))
                     logging.info('Job `%s` from sheep `%s` done', job_id, sheep_id)
+                    
+                    # notify about the finished job
+                    async with self.job_done_condition:
+                        self.job_done_condition.notify_all()
+
                 elif isinstance(message, ErrorMessage):
                     error = ErrorModel({
                         "message": message.message,
@@ -303,11 +308,7 @@ class Shepherd:
                     logging.info('Job `%s` from sheep `%s` failed (%s)', job_id, sheep_id, message.message)
 
                 sheep.in_progress.remove(job_id)
-
-                # notify about the finished job
-                async with self.job_done_condition:
-                    self.job_done_condition.notify_all()
-
+                
     def get_status(self) -> Generator[Tuple[str, SheepModel], None, None]:
         """
         Get status information for all sheep
